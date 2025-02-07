bffc71188481fe91acd514ee0ffd7b18bd0d9f39
Saileeich <109689239+Saileeich@users.noreply.github.com>
2025-02-07 13:07:41 +0100

    Added dataparser to convert to json format

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import requests
-import pyrosm
+import json
+from google.transit import gtfs_realtime_pb2
+from google.protobuf.json_format import MessageToDict
+import pandas as pd
 
 def get_data():
     # Get data from Entur API
-    response = requests.get("https://api.entur.io/realtime/v1/gtfs-rt/vehicle-positions")
+    response = requests.get("https://api.entur.io/realtime/v1/gtfs-rt/vehicle-positions?datasource=KOL")
     
+    # Parse the data to json format
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(response.content)
+
     # Write the data to a file
-    with open("vehicle_positions.pbf", "wb") as file:
-        file.write(response.content)
+    with open("vehicle_positions.json", "w") as file:
+        json.dump(MessageToDict(feed), file, indent=4)
     
     # Return the filepath
-    return "vehicle_positions.pbf"
+    return "vehicle_positions.json"
 
 def read_file(filepath):
-    # Opens file and returns the content
-    with open(filepath, "rb") as file:
-        return file.read()
+    with open(filepath, "r") as file:
+        return json.load(file)
 
-print(read_file(get_data()))
+dataset = read_file(get_data())
+#dataset = read_file("vehicle_positions.json")
+
+fixed_dataset = [x["vehicle"] for x in dataset["entity"]]
+
+df = pd.DataFrame(fixed_dataset)
+print(len(df))
+

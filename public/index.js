6517b089c48d4d522e3f589a4a7e9f2fa039969d
Saileeich <109689239+Saileeich@users.noreply.github.com>
2025-02-20 10:27:08 +0100

    Added cursors to the map

--- public/index.js
+++ public/index.js
@@ -1,7 +1,53 @@
-function myMap() {
-    let mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
-    };
-    let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// Initialize and add the map
+let map;
+let markers = [];
+
+async function initMap() {
+    const position = { lat: 60.3244999609888, lng: 5.37445996887982 };
+
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        zoom: 10,
+        center: position,
+        mapId: "BUS_MAP_ID",
+    });
+
+    updateMarkers();
+    setInterval(updateMarkers, 15000); // Update markers every 15 seconds
 }
+
+async function updateMarkers() {
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    const data = await getData();
+    
+    // Clear existing markers
+    markers.forEach(marker => marker.setMap(null));
+    markers = [];
+
+    for (const vehicle of data) {     
+        if (vehicle["DestinationName"] == "skyss.no") {
+            continue;
+        }
+        
+        const vehiclePosition = { lat: Number(vehicle["Latitude"]), lng: Number(vehicle["Longitude"]) };
+        const marker = new AdvancedMarkerElement({
+            map,
+            position: vehiclePosition,
+            title: `${vehicle["LineNum"]} | ${vehicle["StartName"]} - ${vehicle["DestinationName"]} | ${vehicle["StopPointName"]}`,
+        });
+        markers.push(marker);
+    }
+}
+
+async function getData() {
+    return fetch("http://localhost:3000/api/bus_data")
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        }
+    );
+}
+
+initMap();
